06021fbc69bbc37e1698a1089c4ac71d6dda45df
Daniele Marotta <danielemarotta.developer@gmail.com>
2015-08-06 15:00:51 +0200

    New Utility Module
    Singleton and EventAggregator

---
+++ Assets/Utility/Scripts/EventAggregator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jawa.Utility
+{
+    public class EventAggregator : Singleton<EventAggregator>
+    {
+        public delegate void GameObjectEventHandler<TEvent>(TEvent e) where TEvent : GameObjectEvent;
+
+
+        private IDictionary<Type, IList<object>> _handlersByEventType;
+
+
+        private void Awake()
+        {
+            _handlersByEventType = new Dictionary<Type, IList<object>>();
+        }
+
+
+        public void Trigger<TEvent>(TEvent e) where TEvent : GameObjectEvent
+        {
+            IList<object> handlers;
+
+            if (_handlersByEventType.TryGetValue(e.GetType(), out handlers))
+            {
+                for (var i = handlers.Count - 1; i >= 0; --i)
+                {
+                    var handler = (GameObjectEventHandler<TEvent>)handlers[i];
+
+                    handler(e);
+                }
+            }
+        }
+
+        public void On<TEvent>(GameObjectEventHandler<TEvent> handler) where TEvent : GameObjectEvent
+        {
+            var eventType = typeof(TEvent);
+
+            IList<object> handlers;
+
+            if (!_handlersByEventType.TryGetValue(eventType, out handlers))
+            {
+                handlers = new List<object>();
+
+                _handlersByEventType[eventType] = handlers;
+            }
+
+            handlers.Add(handler);
+        }
+    }
+}
---
+++ Assets/Utility/Scripts/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Jawa.Utility
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        protected static T _instance;
+
+
+        private void OnDestroy()
+        {
+            _instance = null;            
+        }
+
+
+        public static T GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = Object.FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    _instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
