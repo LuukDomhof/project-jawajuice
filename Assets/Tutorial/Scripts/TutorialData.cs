cb81c3007d74fbe7b708e1e4bf631f0ce254c47c
Daniele Marotta <danielemarotta.developer@gmail.com>
2015-08-06 16:37:06 +0200

    Basic Tutorial Framework

---
+++ Assets/Tutorial/Scripts/TutorialData.cs
@@ -0,0 +1,28 @@
+﻿using Jawa.Utility;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jawa.Tutorial
+{
+    public class TutorialData : ScriptableObject
+    {
+        [Serializable]
+        public struct TutorialEntry
+        {
+            public string Name;
+
+            public string Text;
+        }
+
+
+        public TutorialEntry[] Entries;
+
+
+        [MenuItem("Assets/Create/Jawa/Tutorial Data")]
+        public static void CreateAsset()
+        {
+            AssetUtility.CreateAsset<TutorialData>();
+        }
+    }
+}
